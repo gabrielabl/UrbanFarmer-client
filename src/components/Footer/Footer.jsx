210ecff07e84a43fc3c24a20dab547af480b2c08
Gabriela <lopes.gb18@gmail.com>
2023-08-26 13:41:40 -0400

    jsx-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import "./Footer.scss";
 
 const Footer = ()=>{
     return (
-        <></>
+        <ul>
+            <li>© 2023 URBAN FARMER. ALL RIGHTS RESERVED</li>
+            <li><a href="mailto:devteam@example.com">SUPPORT</a></li>
+        </ul>
     );
 };
 
