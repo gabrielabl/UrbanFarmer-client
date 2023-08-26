c16eff11b01db1897c489fcbfcac96ea80d4cd10
Gabriela <lopes.gb18@gmail.com>
2023-08-26 13:33:03 -0400

    jsx-header-added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,30 @@
 import "./Header.scss";
+import logo from '../../Assets/urban-farmer-logo.png';
+import SearchIcon from '@mui/icons-material/Search';
 
 
 const Header = ()=>{
     return(
-        <></>
+        <div>
+
+       <div>
+        <img className='header__logo' src={logo} alt="urban-farmer-logo"></img>
+        <ul>
+            {/* ADD LINK TO PAGES LATER */}
+            <li>PROFILE</li>
+            <li>MY COLLECTION</li>
+            <li>MESSAGES</li>
+        </ul>
+       </div>
+
+       <div>
+     <ul>
+        <li><SearchIcon />Search Items to trade</li>
+        {/* //ADD FUNCTIO TO REMOVE TOKEN FROM SESSION STORAGE AND RE-DIRECT TO HOME PAGE */}
+        <li>SIGN OFF</li>
+     </ul>
+       </div>
+       </div>
     )
 };
 
