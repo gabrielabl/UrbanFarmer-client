8035f2460d14ddb1c48c5a89fe8d8c1fe546ea02
Gabriela <lopes.gb18@gmail.com>
2023-08-29 21:25:51 -0400

    delete-item-added

--- src/pages/MyCollection/MyCollection.jsx
+++ src/pages/MyCollection/MyCollection.jsx
@@ -4,27 +4,22 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import DeleteOutlineTwoToneIcon from '@mui/icons-material/DeleteOutlineTwoTone';
+import DeleteOutlineTwoToneIcon from "@mui/icons-material/DeleteOutlineTwoTone";
 
-const MyCollection = ({ baseURL}) => {
+const MyCollection = ({ baseURL }) => {
   // VARIABLES
   let navigate = useNavigate();
   const [isLoading, setLoading] = useState(true);
-
   const [collectionData, setCollectionData] = useState([]);
-const {id, users_id, item_name, description, item_photo}= collectionData
-
 
   //RETRIEVING TOKEN AND DATA FROM SESSION STORE FOR AUTHORIZATION
   const token = sessionStorage.getItem("token");
   const idUser = sessionStorage.getItem("id");
   const user_name = sessionStorage.getItem("user_name");
 
-
-
   //IF USER DOES NOT HAVE ANY ITEM IN COLLECTION, IT WILL RE-DIRECT TO NEW COLLECTION ITEM PAGE IN THE FUTURE
-  if(collectionData.message){
-    navigate('/profile')
+  if (collectionData.message) {
+    navigate("/profile");
   }
 
   //RETRIEVING USER DATA ACCORDING TO AUTHORIZATION TOKEN
@@ -34,7 +29,7 @@ const {id, users_id, item_name, description, item_photo}= collectionData
       navigate("/login");
     }
 
-      axios
+    axios
       .get(`${baseURL}/profile/${idUser}/collection`, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -45,31 +40,65 @@ const {id, users_id, item_name, description, item_photo}= collectionData
         setLoading(false);
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
         navigate("/login");
       });
-  
-  },[]);
+  }, []);
 
-  if(isLoading){
-   return <div >LOADING..</div>;
+  //DELETE HANDLE
+  const deleteHandle = (id) => {
+    axios
+      .delete(`${baseURL}/collection/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        axios
+          .get(`${baseURL}/profile/${idUser}/collection`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then((res) => {
+            setCollectionData(res.data);
+            setLoading(false);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  return (<>
-  <Header />
-    <section>
- <h1>{user_name}'S COLLECTION</h1>
- <ul>
-{collectionData.map((item)=>( <li> 
-  <img src={item_photo} alt={item_name}></img>
-  <h2>{item.item_name}</h2>
-  <p>{item.description}</p>
-  <button><DeleteOutlineTwoToneIcon /></button>
-</li>))}
+  //WHILE DATA IS NOT RENDERED
+  if (isLoading) {
+    return <div>LOADING..</div>;
+  }
 
- </ul>
-    </section>
-    <Footer />
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>{user_name}'S COLLECTION</h1>
+        <ul>
+          {collectionData.map((item) => (
+            <li key={item.id}>
+              <img src={item.item_photo} alt={item.item_name}></img>
+              <h2>{item.item_name}</h2>
+              <p>{item.description}</p>
+              <button
+                onClick={() => {
+                  deleteHandle(item.id);
+                }}
+              >
+                <DeleteOutlineTwoToneIcon />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <Footer />
     </>
   );
 };
