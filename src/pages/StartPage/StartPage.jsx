bb7ce5462eba3d85f8103f64a0594cfc008afd43
Gabriela <lopes.gb18@gmail.com>
2023-09-02 16:18:30 -0400

    enter-without-login-when-token-present-handle

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -1,15 +1,26 @@
 import AuthHeader from "../../components/AuthHeader/AuthHeader";
 import "./StartPage.scss";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 const StartPage = ({}) => {
+ //VARIABLES
+ let navigate = useNavigate();
+
+const enterHandle = ()=>{
+  const token = sessionStorage.getItem("token");
+  if(token){
+return '/profile'
+  } else {
+return '/login'
+  }
+}
 
   return (
     <>
 <AuthHeader navHeader={'ABOUT'} navUrl={'/about'}    />
       <main>
 <section>
-<Link to={'/login'}><h2 >ENTER</h2></Link>
+<Link to={enterHandle()}><h2 >ENTER</h2></Link>
 <p>NEW TO THE COMMUNITY?</p><Link to={'/signup'} >JOIN HERE.</Link> 
 </section>
       </main>
