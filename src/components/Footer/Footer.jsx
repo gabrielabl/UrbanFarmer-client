1240e4b438d3b7172ece3940f5bd9ad380471dfb
Gabriela <lopes.gb18@gmail.com>
2023-09-03 21:12:43 -0400

    footer-completed-all-screen-sizes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import "./Footer.scss";
 const Footer = ()=>{
     return (
         <footer>
-        <ul>
+        <ul className="footer__container">
             <li>© 2023 URBAN FARMER. ALL RIGHTS RESERVED</li>
-            <li><a href="mailto:devteam@example.com">SUPPORT</a></li>
+            <li><a className="footer__support" href="mailto:devteam@example.com">SUPPORT</a></li>
         </ul>
         </footer>
     );
