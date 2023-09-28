d1fed89a775a33361b7572addd97194be1cd2947
Gabriela <lopes.gb18@gmail.com>
2023-09-28 13:01:29 -0400

    basic-html

--- src/pages/Messages/Messages.jsx
+++ src/pages/Messages/Messages.jsx
@@ -3,6 +3,11 @@ import Loading from "../../components/Loading/Loading";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { useState } from "react";
+import Avatar from "../../components/Avatar/Avatar";
+import DeleteOutlineTwoToneIcon from "@mui/icons-material/DeleteOutlineTwoTone";
+import Button from "../../components/Button/Button";
+import SendIcon from '@mui/icons-material/Send';
+
 
 const Messages = ({ baseURL, setBackground })=>{
     //VARIABLES
@@ -19,6 +24,41 @@ const Messages = ({ baseURL, setBackground })=>{
       <Header />
         
         <main>
+
+            {/* ACTIVE MESSAGE SELECTION */}
+            <section>
+             <h1>MESSAGES</h1>
+             <ul>
+                <li>
+                    <Avatar />
+                    <h2></h2>
+                    <DeleteOutlineTwoToneIcon />
+                </li>
+             </ul>
+            </section>
+    
+    {/* MESSAGE BLOCK */}
+            <div>
+
+                {/* FORMER MESSAGES */}
+                <ul>
+                    <h3></h3>
+                    <div>
+                        <p>
+                        </p>
+                        <p>
+                        </p>
+                    </div>
+                </ul>
+
+          {/* NEW MESSAGE SUBMISSION   */}
+<form>
+    <textarea> </textarea>
+    <Button text="SUBMIT" SVG={<SendIcon/>} />
+
+
+</form>
+            </div>
             
         </main>
 
