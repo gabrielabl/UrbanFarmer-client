5ce88d8248982d290996c797032d8a914851a703
Gabriela <lopes.gb18@gmail.com>
2023-10-07 18:12:23 -0400

    avatar-bug-fix

--- src/components/ActiveMessageList/ActiveMessageList.jsx
+++ src/components/ActiveMessageList/ActiveMessageList.jsx
@@ -7,13 +7,23 @@ const ActiveMessageList =({
   activeMessageHandle,
   baseURL
 }) =>{
+
+  //
+  const avatarPhoto = (receiver, sender) =>{
+    if(!receiver){
+      return sender
+    } else{
+      return receiver
+    }
+  };
+
   return <section className="active-message__wrapper" >
           <h1 className="active-message__title">MESSAGES</h1>
           <ul className="active-message__container">
             {messageData.map(element => <li className="active-message__list-item" onClick={() => {
         activeMessageHandle(element.id);
       }} key={element.id}>
-                <Avatar avatar_source={`${baseURL}/${element.messages[0].receiver_photo}`} avatar_alt={element.messages[0].receiver_name} />
+                <Avatar avatar_source={`${baseURL}/${avatarPhoto(element.messages[0].receiver_photo,element.messages[0].sender_photo)}`} avatar_alt={element.messages[0].receiver_name} />
                 <h2>{element.messages[0].receiver_name? element.messages[0].receiver_name : element.messages[0].sender_name}</h2>
                 <DeleteOutlineTwoToneIcon />
               </li>)}
